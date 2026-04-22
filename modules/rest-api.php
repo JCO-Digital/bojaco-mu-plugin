9134d411dabcded61b4bbdc6f9d8601beb6db4fc
Johan Österberg <jotski86@gmail.com>
2026-04-22 15:49:25 +0300

    Unset all /wp/v2/users endpoints

--- modules/rest-api.php
+++ modules/rest-api.php
@@ -3,9 +3,28 @@
  * This disables the users rest API endpoint for not logged in users.
  */
 
-add_filter( 'rest_endpoints', function ( $endpoints ) {
-	if ( isset( $endpoints['/wp/v2/users'] ) ) {
-		unset( $endpoints['/wp/v2/users'] );
-	}
-	return $endpoints;
-}, 10, 1 );
+/**
+ * Remove user endpoints from REST API for non-logged in users.
+ *
+ * @param array $endpoints The REST API endpoints.
+ *
+ * @return array The modified REST API endpoints.
+ */
+add_filter(
+	'rest_endpoints',
+	function ( $endpoints ) {
+		if ( is_user_logged_in() || ! is_array( $endpoints ) ) {
+			return $endpoints;
+		}
+
+		foreach ( array_keys( $endpoints ) as $route ) {
+			if ( str_starts_with( $route, '/wp/v2/users' ) ) {
+				unset( $endpoints[ $route ] );
+			}
+		}
+
+		return $endpoints;
+	},
+	10,
+	1
+);
